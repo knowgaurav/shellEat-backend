1bb87d9ec6cb4317b74a8d68daee4fd05a4aa4b1
Gaurav Singh <sgaurav497@gmail.com>
2023-08-27 22:03:36 +0530

    Added cors

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from "express";
 import authRoutes from "./routes/auth.js";
 import transactionsRoutes from "./routes/transactions.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionsRoutes);
